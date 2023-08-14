ec9b05555d75b238eb048b5d5dc12258304f565c
Joshy Jose <joshy.jose@huld.io>
2023-08-14 12:13:07 +0200

    boost test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "iostream"
+#include <boost/thread.hpp>
 
 int main() {
     std::cout << "hello world!" << std::endl;
+        std::cout << boost::thread::hardware_concurrency() << std::endl;
 }
